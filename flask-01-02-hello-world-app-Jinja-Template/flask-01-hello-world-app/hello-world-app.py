5c37facdbb486f44d682e24b2fee871fbdd72f25
fatihtepe <79118602+fatihtepe@users.noreply.github.com>
2021-06-19 19:40:04 -0400

    Update hello-world-app.py

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -4,23 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+    return 'Hello World!'
 
 @app.route('/second')
 def second():
-	return 'This is the second page'
+    return 'No Pain No Gain'
 
 @app.route('/third/subthird')
 def third():
-	return 'Be warrior, not worrier!'
+    return 'This is the subpage of third page'
 
 @app.route('/forth/<string:id>')
 def forth(id):
-	return f'Id number of this page is {id}'
-
-
-
+    return f'Id number of this page is {id}'
 
 if __name__ == '__main__':
-	# app.run(debug = True)
-	app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
