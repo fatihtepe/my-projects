8839e768e96f713976a930a5130471c0ff1b5767
Fatih TEPE <79118602+fatihtepe@users.noreply.github.com>
2021-08-23 14:26:04 -0400

    Delete Project-200-Roman-Numerals-Converter directory

--- Project-200-Roman-Numerals-Converter/userdata.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-yum update -y
-yum install python3 -y
-pip3 install flask
-cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/fatihtepe/my-projects/main/Project-200-Roman-Numerals-Converter/templates/index.html
-wget -P templates https://raw.githubusercontent.com/fatihtepe/my-projects/main/Project-200-Roman-Numerals-Converter/templates/result.html
-wget https://raw.githubusercontent.com/fatihtepe/my-projects/main/Project-200-Roman-Numerals-Converter/app.py
-python3 app.py`
