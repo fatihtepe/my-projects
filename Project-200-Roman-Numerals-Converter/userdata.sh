6674ed6e791c833f245e816ad00b469e827ccdc9
Fatih Tepe <fatihtepe@gmail.com>
2021-08-23 13:16:42 -0400

    Project-200 Kittens-carousel static website ec2

---
+++ Project-200-Roman-Numerals-Converter/userdata.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+cd /home/ec2-user
+wget -P templates https://raw.githubusercontent.com/fatihtepe/my-projects/main/Project-001-Roman-Numerals-Converter/templates/index.html
+wget -P templates https://raw.githubusercontent.com/fatihtepe/my-projects/main/Project-001-Roman-Numerals-Converter/templates/result.html
+wget https://raw.githubusercontent.com/fatihtepe/my-projects/main/Project-001-Roman-Numerals-Converter/app.py
+python3 app.py`
