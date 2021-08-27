d5293d3240d42aa903505d4da84af4b874abb9c8
Fatih Tepe <fatihtepe@gmail.com>
2021-08-27 14:16:25 -0400

    This infrastructure has Application Load Balancer with Auto Scaling Group of Elastic Compute Cloud (EC2) Instances and Relational Database Service (RDS) on defined VPC

--- Project-500-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
+++ Project-500-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
+TOKEN="ghp_KFHKeXVakOt4T1Eu7"
 git clone https://$TOKEN@github.com/fatihtepe/tepe-aws-capstone.git
 cd /home/ubuntu/tepe-aws-capstone
 apt install python3-pip -y
